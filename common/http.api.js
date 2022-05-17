bf2efe87be04cd7ca0bbfec31eaf8d2fddf3a80b
Zyzdevr <103498071+Zyzdevr@users.noreply.github.com>
2022-05-17 11:34:41 +0800

    api

---
+++ common/http.api.js
@@ -0,0 +1,13 @@
+// /common/http.api.js
+
+// 此处第二个参数vm，就是我们在页面使用的this，你可以通过vm获取vuex等操作，更多内容详见uView对拦截器的介绍部分：
+const install = (Vue, vm) => {
+	vm.$u.api = {};
+	// 此处没有使用传入的params参数
+	vm.$u.api.getIndexData = (params = {}) => vm.$u.get('api/index', params);
+	// 将各个定义的接口名称，统一放进对象挂载到vm.$u.api(因为vm就是this，也即this.$u.api)下
+}
+
+export default {
+	install
+}
---
+++ common/http.interceptor.js
@@ -0,0 +1,51 @@
+const install = (Vue, vm) => {
+	Vue.prototype.$u.http.setConfig({
+		baseUrl: '', // 请求的本域名
+		dataType: 'json',
+		showLoading: true, // 是否显示请求中的loading
+		loadingText: '请求中...', // 请求loading中的文字提示
+		loadingTime: 800, // 在此时间内，请求还没回来的话，就显示加载中动画，单位ms
+		originalData: true, // 是否在拦截器中返回服务端的原始数据
+		loadingMask: true, // 展示loading的时候，是否给一个透明的蒙层，防止触摸穿透
+	});
+
+	// 请求拦截部分，如配置，每次请求前都会执行
+	Vue.prototype.$u.http.interceptor.request = (config) => {
+		config.header.Authorization = 'Bearer'+'xxxxxx';
+		return config;
+		// 如果return一个false值，则会取消本次请求
+		// if(config.url == '/user/rest') return false; // 取消某次请求
+	}
+
+	// 响应拦截，如配置，每次请求结束都会执行本方法
+	Vue.prototype.$u.http.interceptor.response = (res) => {
+		if (res.statusCode == 200) {
+			vm.$u.toast('请求成功 ');
+			return res.data;
+		} else if (res.statusCode == 401) {
+			// 假设201为token失效，这里跳转登录
+			vm.$u.toast('验证失败，请重新登录');
+			setTimeout(() => {
+				// 此为uView的方法，详见路由相关文档
+				vm.$u.route('/pages/user/login')
+			}, 1500)
+			return false;
+		} else {
+			// 如果返回false，则会调用Promise的reject回调，
+			// 并将进入this.$u.post(url).then().catch(res=>{})的catch回调中，res为服务端的返回值
+			return false;
+		}
+	}
+	// 模拟一个patch
+	vm.$u.patch = (url,params={}) => {
+		const _params = {
+			...params,
+			_method: 'PATCH'
+		}
+		return vm.$u.post(url,_params)
+	}
+}
+
+export default {
+	install
+}
